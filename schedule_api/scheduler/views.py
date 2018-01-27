c9b8271b5eaa956e269cf172e33be33f1ffc5843
Leonardo A.  Vanzella <leovanzella@gmail.com>
2018-01-27 19:21:24 -0200

    Now the post method can check for consistency, just checking for possible overlaping appointments

--- schedule_api/scheduler/views.py
+++ schedule_api/scheduler/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, status
 from .models import Patient, Appointment
 from .serializers import PatientSerializer, AppointmentSerializer
+from rest_framework.response import Response
 
 ''' Return a list of all patients with GET
     This class can also create a new Patient with POST '''
@@ -24,6 +25,33 @@ class AppointmentList(generics.ListCreateAPIView):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
+    def consistence_is_ok(self, date, start_time, end_time):
+        good_appointment = True
+        appointment = Appointment.objects.filter(
+            date=date, start_time__lte=start_time).order_by('-start_time')
+
+        if appointment.exists():
+            # at least 1 appointment has been found before
+            close_appointment = appointment.first()
+            # Check if the appointment before overlaps the start time
+            if close_appointment.end_time > start_time:
+                good_appointment = False
+
+        # get the appointments that start after the requested one
+        appointment = Appointment.objects.filter(
+            date=date, start_time__gt=start_time).order_by('start_time')
+
+        if appointment.exists():
+            # at least 1 appointment has been found after
+            close_appointment = appointment.first()
+            # if the end time of the requested appointment is later
+            # than the start time of the next appointment,
+            # it cannot be scheduled
+            if end_time > close_appointment.start_time:
+                good_appointment = False
+
+        return good_appointment
+
     ''' Check POST method for consistences '''
     def post(self, request, *args, **kwargs):
 
@@ -38,7 +66,11 @@ class AppointmentList(generics.ListCreateAPIView):
                 return Response({'error': 'Start time must be earlier than end time.'},
                     status=status.HTTP_400_BAD_REQUEST)
 
-        return self.create(request, *args, **kwargs)
+            ''' Here overlaps can be identified for consistence  '''
+            if self.consistence_is_ok(date, start_time, end_time):
+                return self.create(request, *args, **kwargs)
+            else:
+                return Response({'error': 'Bad appointment.'}, status=status.HTTP_400_BAD_REQUEST)
 
 ''' Return a list of described appointments with GET
     This method can also update an appointment with PUT 
