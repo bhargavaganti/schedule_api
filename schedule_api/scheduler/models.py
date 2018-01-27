064c1a7dd170470d6c36be572f77e18328ccb3d6
Leonardo A.  Vanzella <leovanzella@gmail.com>
2018-01-27 08:52:20 -0200

    Created model for 'Patient's, because all appointments will be defined by a patient

--- schedule_api/scheduler/models.py
+++ schedule_api/scheduler/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+
+    class Meta:
+
+        db_table = 'patient'
+
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    id_number = models.IntegerField()
+
+    def __str__(self):
+        return self.name
