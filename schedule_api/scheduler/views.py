f7b8edc62aff30048b8455f5a70444f6e9af2cd2
Leonardo A.  Vanzella <leovanzella@gmail.com>
2018-01-27 18:13:44 -0200

    Comment view classes

--- schedule_api/scheduler/views.py
+++ schedule_api/scheduler/views.py
@@ -1,22 +1,32 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import Patient, Appointment
 from .serializers import PatientSerializer, AppointmentSerializer
 
+''' Return a list of all patients with GET
+    This class can also create a new Patient with POST '''
 class PatientList(generics.ListCreateAPIView):
 
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
 
+''' Return a list of described patient with GET
+    This method can also update a  Patient with PUT 
+    or delete with DELETE '''
 class PatientDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
 
+''' Return a list of all appointments with GET
+    This class can also create a new appointment with POST '''
 class AppointmentList(generics.ListCreateAPIView):
 
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
+''' Return a list of described appointments with GET
+    This method can also update an appointment with PUT 
+    or delete with DELETE '''
 class AppointmentDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Appointment.objects.all()
