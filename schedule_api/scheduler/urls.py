2b89029e83204af837d7728e7ebf0e5d85b78bc1
Leonardo A.  Vanzella <leovanzella@gmail.com>
2018-01-27 11:05:13 -0200

    Add route for Patien's Detais and Appointment's Detais

--- schedule_api/scheduler/urls.py
+++ schedule_api/scheduler/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     re_path(r'^patients/$', views.PatientList.as_view(), name='patient-list'),
+    re_path(r'^patient/(?P<pk>[0-9]+)/$', views.PatientDetail.as_view(), name='patient-detail'),
+    re_path(r'^appointments/$', views.AppointmentList.as_view(), name='appointment-list'),
+    re_path(r'^appointment/(?P<pk>[0-9]+)/$', views.AppointmentDetail.as_view(), name='appointment-detail'),
 ]
