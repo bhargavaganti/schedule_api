12384040c1aba004f4bca1e50f7a29cf6076b7df
Leonardo A.  Vanzella <leovanzella@gmail.com>
2018-01-27 11:10:51 -0200

    Class RetrieveUpdateDestroyAPIView retrieves details from Patients and Appointments

--- schedule_api/scheduler/views.py
+++ schedule_api/scheduler/views.py
@@ -1,8 +1,23 @@
 from rest_framework import generics
-from .models import Patient
-from .serializers import PatientSerializer
+from .models import Patient, Appointment
+from .serializers import PatientSerializer, AppointmentSerializer
 
 class PatientList(generics.ListCreateAPIView):
 
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
+class PatientDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+class AppointmentList(generics.ListCreateAPIView):
+
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+class AppointmentDetail(generics.RetrieveUpdateDestroyAPIView):
+
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
