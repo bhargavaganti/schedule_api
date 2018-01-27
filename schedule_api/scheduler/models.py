5b4fc997b6488108f6563a0cc31845e55c6a6b91
Leonardo A.  Vanzella <leovanzella@gmail.com>
2018-01-27 10:57:17 -0200

    Updated scheduler models with Appointment class. Now Patient has appointment and id_number is used as Primary Key for DB.

--- schedule_api/scheduler/models.py
+++ schedule_api/scheduler/models.py
@@ -8,7 +8,29 @@ class Patient(models.Model):
 
     name = models.CharField(max_length=255)
     age = models.IntegerField()
-    id_number = models.IntegerField()
+    id_number = models.IntegerField(primary_key=True)
 
     def __str__(self):
         return self.name
+
+
+class Appointment(models.Model):
+
+    class Meta:
+
+        db_table = 'scheduling'
+
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    patient = models.ForeignKey('Patient', on_delete=models.CASCADE, related_name='appointment')
+    procedure = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str('Appointment for %s at %s %s %s' % (self.patient,
+                                                       self.date,
+                                                       self.start_time,
+                                                       self.end_time,
+                                                       self.procedure))
+
+
