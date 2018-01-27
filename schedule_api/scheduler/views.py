432303b60d1d0c4821d58099b25a6e4a801089db
Leonardo A.  Vanzella <leovanzella@gmail.com>
2018-01-27 09:02:42 -0200

    Create a new View for patient's list. Used generic class-based Views , because all GET, POST, PUT, PATCH and DELETE methods are already implemented. If its required that HTTP methods should have further implementation, it won't be a problem.

--- schedule_api/scheduler/views.py
+++ schedule_api/scheduler/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Patient
+from .serializers import PatientSerializer
 
-# Create your views here.
+class PatientList(generics.ListCreateAPIView):
+
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
